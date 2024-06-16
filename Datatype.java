39547af78aa94ab04fb3934d911389f30d326b18
Prashant Chaudhary <114799837+prashantkc09@users.noreply.github.com>
2024-06-17 01:42:50 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,24 @@
+public class Datatype {
+    public static void main(String[] args){
+        int myNumber =865;
+        int yourNumber=0;
+        System.out.println(myNumber);
+        yourNumber=45;
+        System.out.println(yourNumber);
+
+        float myFloat = 5.0f;
+        System.out.println(myFloat);
+
+        boolean isVegetarian = true;
+        System.out.println(isVegetarian);
+
+        String wishes="Good Morning";
+        System.out.println(wishes);
+
+        char myCharacter = 'K';
+        System.out.println(myCharacter);
+
+
+
+    }
+}
